f067a931a5ca04da5fd5b3d590aea75d11cff00c
amenyenudaniel <amenyenudaniel321@gmail.com>
2023-08-10 14:35:06 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/Header/Header";
 import TV from "./components/TV/TV";
 import Shows from "./components/Shows/Shows";
 import Kids from "./components/Kids/Kids";
 import Footer from "./components/Footer/Footer";
+
 const App = () => {
+  useEffect(() => {
+    alert("Hey Note! This is a clone website");
+  });
   return (
     <div>
       <Header />
